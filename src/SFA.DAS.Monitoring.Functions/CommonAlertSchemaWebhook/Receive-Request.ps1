730de81bc1d24b728c0e78e1e896d5c1c4e0892c
Craig Gumbley <craiggumbley@gmail.com>
2019-11-12 20:12:08 +0000

    Ensure the whole payload is captured

--- src/SFA.DAS.Monitoring.Functions/CommonAlertSchemaWebhook/Receive-Request.ps1
+++ src/SFA.DAS.Monitoring.Functions/CommonAlertSchemaWebhook/Receive-Request.ps1
@@ -11,7 +11,7 @@ try {
 
     # Test-RequestHeaders -Headers $Request.Headers
     Test-RequestBody -Body $Request.Body
-    Write-Information ($Request.Body | ConvertTo-Json)
+    Write-Information ($Request.Body | ConvertTo-Json -Depth 10)
 
     $AlertData = $Request.Body.data
     $Essentials = $AlertData.essentials
