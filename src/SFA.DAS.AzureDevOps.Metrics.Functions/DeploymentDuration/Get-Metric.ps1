01ee83c9ee753880b48c67b17181e5ee1ebd805b
Craig Gumbley <craiggumbley@gmail.com>
2020-01-19 00:04:29 +0000

    Remove EA setting

--- src/SFA.DAS.AzureDevOps.Metrics.Functions/DeploymentDuration/Get-Metric.ps1
+++ src/SFA.DAS.AzureDevOps.Metrics.Functions/DeploymentDuration/Get-Metric.ps1
@@ -1,6 +1,5 @@
 param($Timer, $TriggerMetadata)
 
-$ErrorActionPreference = "Stop"
 if ($ENV:ENVIRONMENTNAME -notin @($ENV:AZURE_DEVOPS_METRIC_ENVIRONMENTS.Split(","))) {
     Write-Information "The function is not configured to run in this environment."
     return
