c4c962f7b7866f0a59c70448e29c3baef981145e
NickGraham101 <nick.graham101@googlemail.com>
2020-09-30 14:07:20 +0100

    Revert "Added logging to confirm PSVersion in use"
    This reverts commit ec0e9a007bd8bd3554ac1e92dc7c3cfc3f244890.

--- src/SFA.DAS.AzureDevOps.Metrics.Functions/BuildDuration/Get-Metric.ps1
+++ src/SFA.DAS.AzureDevOps.Metrics.Functions/BuildDuration/Get-Metric.ps1
@@ -2,8 +2,6 @@ param($Timer, $TriggerMetadata)
 
 if ($ENV:ENVIRONMENTNAME -notin @($ENV:AZURE_DEVOPS_METRIC_ENVIRONMENTS.Split(","))) {
     Write-Information "The function is not configured to run in this environment."
-    ##TO DO: remove this
-    Write-Information "This environment is running PSVersion $($PSVersionTable.PSVersion.ToString())"
     return
 }
 
